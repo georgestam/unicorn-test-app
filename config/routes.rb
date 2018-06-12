b69641d89cb053c9b5f928c83adc5ad9621117d1
Jordi Fernandez <jordi@ywarp.com>
2018-06-12 18:02:58 +0100

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :avatars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Blorgh::Engine, at: "/blog"
+  
+  root to: "home#index"
 end
